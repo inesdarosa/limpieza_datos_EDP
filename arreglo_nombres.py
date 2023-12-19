efb4b7251f1fea2d20e6dc64d8e175f4b517e2ec
inesdarosa <inespossible@gmail.com>
2023-12-19 20:31:57 -0300

    agregue evaluacion

--- arreglo_nombres.py
+++ arreglo_nombres.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd 
 import os
 import re
+import unidecode
 
 os.chdir('G:/Mi unidad/proy_silvana/planillas/Insc_EVAI_CSV_2/inscripcion')
 
@@ -59,3 +60,64 @@ new_insc_general_ordenada['Nombre y Apellido'].head(30)
 new_insc_general_ordenada.to_csv('new_insc_general_2.csv')
 
 
+# Lo que sigue para EVALUACION
+
+os.chdir('G:/Mi unidad/proy_silvana/planillas/Insc_EVAI_CSV_2/evaluacion')
+
+Eval_18_19_20_21_mod = pd.read_csv('Eval_18_19_20_21_mod.csv')
+Eval_18_19_20_21_mod.columns.values
+
+# Con esto elimino tildes, ñ y algún otro chirimbolo:
+Eval_18_19_20_21_mod['Nombre y Apellido'] = (Eval_18_19_20_21_mod['Nombre y Apellido'].
+                                      apply(lambda x: unidecode.unidecode(x)))
+
+
+# Acá paso todos los 'Nombre y Apellido' a minúsculas:
+cols_to_change = ['Nombre y Apellido']
+
+for col in cols_to_change:
+    Eval_18_19_20_21_mod['Nombre y Apellido'] = (Eval_18_19_20_21_mod['Nombre y Apellido']
+                                             .str.lower())
+
+
+
+# con este ciclo for, a través del email, arreglo el nombre
+# de los estudiantes para que los que tengan el mismo email
+# aparezcan con el mismo Nombre y Apellido
+
+len(Eval_18_19_20_21_mod['Dirección de correo electrónico'])
+Eval_18_19_20_21_mod['Nombre y Apellido'].head(30)
+Eval_18_19_20_21_mod['Nombre y Apellido'].tail(30)
+
+for i in range(len(Eval_18_19_20_21_mod['Dirección de correo electrónico'])-1):
+    
+    if Eval_18_19_20_21_mod['Dirección de correo electrónico'][i] == (Eval_18_19_20_21_mod
+    ['Dirección de correo electrónico'][i + 1]) and Eval_18_19_20_21_mod['Nombre y Apellido'][i] != (
+        Eval_18_19_20_21_mod['Nombre y Apellido'][i + 1]):
+
+        Eval_18_19_20_21_mod['Nombre y Apellido'][i + 1] = (
+            Eval_18_19_20_21_mod['Nombre y Apellido'][i])
+
+Eval_18_19_20_21_mod.to_csv('Eval_18_19_20_21_mod_1.csv')
+
+
+# Ahora voy a tratar de modificar el nombre de los cursos
+
+eval_cursos = pd.read_csv('eval_cursos_editado.csv')
+eval_cursos.columns.values
+
+for i in range(0, len(Eval_18_19_20_21_mod[
+    '¿Cual fue el curso o actividad de Educación Permanente que realizaste?'])):
+
+    for j in range(0, len(eval_cursos['cursos_originales'])):
+
+        if Eval_18_19_20_21_mod[
+            '¿Cual fue el curso o actividad de Educación Permanente que realizaste?'][i] == (
+            eval_cursos['cursos_originales'][j]):
+
+            Eval_18_19_20_21_mod[
+                '¿Cual fue el curso o actividad de Educación Permanente que realizaste?'][i] = (
+                eval_cursos['cursos_corregidos'][j])
+
+
+Eval_18_19_20_21_mod.to_csv('Eval_18_19_20_21_mod_2.csv')
